0b70ff161a06e6a37b64024d0ebcc497d66708c9
alamasyarie <maiaelarsya@gmail.com>
2021-05-02 09:04:21 +0700

    Create add book component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import AddBook from "./components/Books/addBooks";
 
 function App() {
   return (
     <div className="App">
       <h1>Book App</h1>
+      <AddBook />
     </div>
   );
 }
