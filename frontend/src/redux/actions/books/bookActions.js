956c37a5058073b4ef01a9ed3be4917093c91018
alamasyarie <maiaelarsya@gmail.com>
2021-05-02 08:31:14 +0700

    Setup redux for state management

---
+++ frontend/src/redux/actions/books/bookActions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import {
+  CREATE_BOOK_FAIL,
+  CREATE_BOOK_REQUEST,
+  CREATE_BOOK_SUCCESS,
+} from "../actionTypes";
+
+const createBookActions = bookData => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: CREATE_BOOK_REQUEST,
+      });
+
+      const config = {
+        "Content-Type": "application/json",
+      };
+
+      const res = await axios.post("/api/books", bookData, config);
+
+      dispatch({
+        type: CREATE_BOOK_SUCCESS,
+        payload: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CREATE_BOOK_FAIL,
+        payload: err.response && err.response.data.message,
+      });
+    }
+  };
+};
+
+export { createBookActions };
---
+++ frontend/src/redux/store/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { createBookReducer } from "../reducers/books/createBooksReducers";
+
+const middlewares = [thunk];
+
+const reducer = combineReducers({
+  bookCreated: createBookReducer,
+});
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+export { store };
